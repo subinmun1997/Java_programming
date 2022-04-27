841a1d7c72906615335204df5edc1f9e1dc8bc0b
subinmun1997 <qzxy812@gmail.com>
2022-04-27 18:28:05 +0900

    variable

---
+++ src/Chapter1/Solution1.java
@@ -0,0 +1,17 @@
+package Chapter1;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        int year = 0;
+        int age = 14;
+
+        System.out.println("age = " + age);
+        System.out.println("year = " + year);
+
+        year = age + 2000;
+        age = age + 1;
+
+        System.out.println("age = " + age);
+        System.out.println("year = " + year);
+    }
+}
